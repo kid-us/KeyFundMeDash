2d5ef15de8fab6f2e1bea61ea7a34e697792e5b5
kid-us <kidushh29@gmail.com>
2024-11-28 18:11:25 +0300

    Home page fixed

--- src/components/Pages/Home.tsx
+++ src/components/Pages/Home.tsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
-import Fundraiser from "../Home/Fundraiser";
 import useDocumentTitle from "../../hooks/useDocumentTitle";
+import Sidebar from "../Sidebar/Sidebar";
+import Navbar from "../Navbar/Navbar";
 
 const Home = () => {
   const [title] = useState("KeyFundMe | Dashboard");
   useDocumentTitle(title);
 
   return (
-    <div>
-      <p>Welcome</p>
-      <Fundraiser />
+    <div className="relative lg:grid md:grid grid-cols-11 lg:bg-[#F8F8F8]">
+      {/* Sidebar */}
+      <div className="lg:col-span-2 lg:block hidden w-full">
+        <Sidebar />
+      </div>
+      <div className="lg:col-span-9 md:col-span-10 lg:px-8 lg:py-9 p-3">
+        <Navbar />
+      </div>
     </div>
   );
 };
