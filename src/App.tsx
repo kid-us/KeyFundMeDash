e5f184e92e202965128826257bbaefb81fe0d8bd
kid-us <kidushh29@gmail.com>
2024-11-28 18:12:02 +0300

    Routes completed.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import "./App.css";
 import Home from "./components/Pages/Home";
 import Login from "./components/Pages/Login";
 import PageNotFound from "./components/Pages/PageNotFound";
+import Fundraising from "./components/Pages/Fundraising";
+import Setting from "./components/Pages/Setting";
 // import Protected from "./components/Protected/Protected";
 
 function App() {
@@ -14,6 +16,22 @@ function App() {
           element={
             // <Protected>
             <Home />
+            // </Protected>
+          }
+        />
+        <Route
+          path="/fundraising"
+          element={
+            // <Protected>
+            <Fundraising />
+            // </Protected>
+          }
+        />
+        <Route
+          path="/setting"
+          element={
+            // <Protected>
+            <Setting />
             // </Protected>
           }
         />
